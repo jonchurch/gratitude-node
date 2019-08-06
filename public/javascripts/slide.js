eb40751522da88b54bfd345a1e1e4de199b80b5b
Nadeau <ez17974@bell.corp.bce.ca>
2019-08-06 16:29:27 -0300

    Jquery slides

--- public/javascripts/slide.js
+++ public/javascripts/slide.js
@@ -1,9 +1,17 @@
-function slideElement(elementToSlide, direction, speed){
+// function slideElement(elementToSlide, direction, speed){
+//     //slide element from left
+//     console.log("slide element:"+elementToSlide);
+//     console.log("slide direction:"+direction);
+//     console.log("slide speed:"+speed);
+//      elementToSlide.slideDown("slow");
+  
+// }
+function slideElement(classToSlide){
     //slide element from left
-    console.log("slide element:"+elementToSlide);
-    console.log("slide direction:"+direction);
-    console.log("slide speed:"+speed);
-     elementToSlide.slideDown("slow");
+    console.log("slide element:"+classToSlide);
+    // console.log("slide direction:"+direction);
+    // console.log("slide speed:"+speed);
+     //elementToSlide.slideDown("slow");
   
 }
-        
+            
