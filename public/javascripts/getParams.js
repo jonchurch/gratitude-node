69409a44f9a5bdc0223106d697c8d5e573863b1e
Nadeau <ez17974@bell.corp.bce.ca>
2019-08-09 22:43:26 -0300

    Fixed form on homepage and starting sub pages

--- public/javascripts/getParams.js
+++
@@ -1,7 +0,0 @@
-// method to get param(s) and return to call
-
-$.urlParam = function(name){
-    console.log("param: "+name);
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    return results[1] || 0;
-}
